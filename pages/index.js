dce78b11780578979670fca507f36b2e00be1f9d
sundicide <kyusuen@gmail.com>
2021-03-17 08:33:12 +0900

    feat: add search and filter

--- pages/index.js
+++ pages/index.js
@@ -3,27 +3,35 @@ import React from 'react';
 import styles from '../styles/Home.module.css'
 
 class Problems {
+  tag = []
   question = ''
   answer = ''
-  constructor(question, answer) {
+  constructor(tag, question, answer) {
+    this.tag = tag
     this.question = question
     this.answer = answer
   }
 }
 
 const problemsList = [
-  new Problems('drink, water / good, health', 'I think that drinking enough water is good for health.'),
-  new Problems('bonus good / outstanding employee', 'In my opinion, bonuses are effective rewards for outstanding employees.'),
-  new Problems('creativity / painter', 'I agree that creativity is the most important quality for a painter.'),
-  new Problems('children / advice / parent', 'I agree that children need to get advice from their parents.'),
-  new Problems('children / advice / parent', 'I agree that children need to get advice from their parents.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'),
+  new Problems(['water', 'health'], 'drink, water / good, health', 'I think that drinking enough water is good for health.'),
+  new Problems(['in-my-opinion'], 'bonus good / outstanding employee', 'In my opinion, bonuses are effective rewards for outstanding employees.'),
+  new Problems(['i-agree'], 'creativity / painter', 'I agree that creativity is the most important quality for a painter.'),
+  new Problems(['i-agree'], 'children / advice / parent', 'I agree that children need to get advice from their parents.'),
+  new Problems(['i-agree'], 'students / mobile phones / classroom', "I disagree that students shouldn't be allowed to have mobile phones in the classroom."),
+  new Problems(['i-agree'], 'studying abroad / young age', "I disagree that studying abroad at a young age is good."),
+  new Problems(['i-agree', 'cloth'], 'buying cloth / shop', "I prefer buying my clothes in a shop to shopping on the Internet."),
 ]
 
 export default function Home() {
   const [localProblemLists, setLocalProblemLists] = React.useState([...problemsList]);
+  const getTags = () => problemsList.reduce((accu, curr) => {
+    curr.tag.forEach(t => accu.add(t))
+    return accu
+  }, new Set())
 
 
-  function doInit() {
+  const doInit = () => {
     setLocalProblemLists(problemsList)
   }
   const doRaffle = () => {
@@ -35,13 +43,24 @@ export default function Home() {
     }
     setLocalProblemLists(newProblems)
   }
+  const doSearch = (e) => {
+    const newList = problemsList.filter(d => d.answer.includes(e.target.value) )
+    setLocalProblemLists(newList)
+  }
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {doSearch(e)}
+  }
   const hideElem = (i) => {
     const newProblems = [...localProblemLists]
     newProblems.splice(i, 1)
     setLocalProblemLists(newProblems)
   }
+  const filterKey = (e) => {
+    const newProblems = [...problemsList]
+    setLocalProblemLists(newProblems.filter(d => d.tag.includes(e.target.dataset.value)));
+  }
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onKeyDown={handleKeyDown}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
@@ -52,10 +71,24 @@ export default function Home() {
           <button onClick={doInit}>Init</button>
           <button onClick={doRaffle}>Raffle</button>
         </div>
+        <section className={styles.searches}>
+          <input onChange={doSearch} type="text"/>
+          <button>search</button>
+        </section>
+        <section className={styles.tags}>
+          {Array.from(getTags()).map(key => (
+            <div key={key} onClick={filterKey} data-value={key}>
+              {key}
+            </div>
+          ))}
+        </section>
         <div className={styles.grid}>
           {localProblemLists.map((d, i) => (
             <div key={i} className={styles.card}>
               <button onClick={() => hideElem(i)}>hide</button>
+              <div>
+                tags: {d.tag.toString()}
+              </div>
               <a href="#">
                 <h3>{d.question}</h3>
               </a>
