7d9e5fe28706b5019bac5bdd5e1db4e66f14b625
sundicide <kyusuen@gmail.com>
2021-03-26 06:26:00 +0900

    feat: add fontawesome and fix styles of login

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/globals.css'
 import { AuthProvider } from '../auth';
+import { library } from '@fortawesome/fontawesome-svg-core'
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+// import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faHome)
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/login.js
+++ pages/login.js
@@ -1,27 +1,44 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { firebaseClient } from '../firebaseClient';
+import styles from '../styles/Login.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default (_props) => {
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
   return (
-    <div>
-      <Link href="/">
-        <a>Go back to home page</a>
-      </Link>
-      <br />
-      <input
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder={'Email'}
-      />
-      <input
-        type={'password'}
-        value={pass}
-        onChange={(e) => setPass(e.target.value)}
-        placeholder={'Password'}
-      />
+    <div className={styles.root}>
+      <div className={styles.wrapper}>
+        <Link href="/">
+          <FontAwesomeIcon className={styles.icon} icon='home' />
+        </Link>
+        <div className={styles.inputWrapper}>
+          <span>ID: </span>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder={'Email'}
+          />
+        </div>
+        <div className={styles.inputWrapper}>
+          <span>PW: </span>
+          <input
+            type={'password'}
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
+            placeholder={'Password'}
+          />
+        </div>
+        <button
+          onClick={async () => {
+            await firebaseClient.auth().signInWithEmailAndPassword(email, pass);
+            window.location.href = '/';
+          }}
+        >
+          Log in
+        </button>
+      </div>
       {/* <button
         onClick={async () => {
           await firebaseClient
@@ -32,14 +49,6 @@ export default (_props) => {
       >
         Create account
       </button> */}
-      <button
-        onClick={async () => {
-          await firebaseClient.auth().signInWithEmailAndPassword(email, pass);
-          window.location.href = '/';
-        }}
-      >
-        Log in
-      </button>
     </div>
   );
 };
