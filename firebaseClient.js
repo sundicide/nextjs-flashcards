ba5801f84088871378740de411df650403bfaac9
sundicide <kyusuen@gmail.com>
2021-04-02 09:29:59 +0900

    fix: firebaseclient using env

--- firebaseClient.js
+++ firebaseClient.js
@@ -2,33 +2,16 @@ import firebaseClient from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-/*
-Copy/paste your *client-side* Firebase credentials below.
-To get these, go to the Firebase Console > open your project > Gear Icon >
-Project Settings > General > Your apps. If you haven't created a web app
-already, click the "</>" icon, name your app, and copy/paste the snippet.
-Otherwise, go to Firebase SDK Snippet > click the "Config" radio button >
-copy/paste.
-*/
 const CLIENT_CONFIG = {
-  apiKey: "AIzaSyAQrscuacML1TB1d0LhO7NSpRbzae2Q5wM",
-  authDomain: "nextjs-postcards.firebaseapp.com",
-  projectId: "nextjs-postcards",
-  storageBucket: "nextjs-postcards.appspot.com",
-  messagingSenderId: "971229343442",
-  appId: "1:971229343442:web:391466e161a3e83eb5c70b",
-  measurementId: "G-9T0WL40N4S"
+  apiKey: process.env["API_KEY"],
+  authDomain: process.env["AUTH_DOMAIN"],
+  projectId: process.env["PROJECT_ID"],
+  storageBucket: process.env["STORAGE_BUCKET"],
+  messagingSenderId: process.env["MESSAGING_SENDER_ID"],
+  appId: process.env["APP_ID"],
+  measurementId: process.env["MEASUREMENT_ID"]
 };
 
-// if (!firebaseClient.apps.length) {
-//   firebaseClient.initializeApp(CLIENT_CONFIG);
-//   firebaseClient
-//     .auth()
-//     .setPersistence(firebaseClient.auth.Auth.Persistence.SESSION);
-//   firebaseClient.analytics();
-// }
-
-// export { firebaseClient };
 if (typeof window !== "undefined" && !firebaseClient.apps.length) {
   firebaseClient.initializeApp(CLIENT_CONFIG);
   firebaseClient
